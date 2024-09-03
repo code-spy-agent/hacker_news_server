2e73cf1c5dd16240bc426434d8857d562267df4c
code-spy-agent <veeraagentic.ai@gmail.com>
2024-09-03 12:43:43 +0530

    cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const port = 3000;
 
 app.use(cors({
-  origin: 'http://127.0.0.1:5500'
+  origin: '*' // Allow all origins during development
 }));
 
 // Serve static files from the 'public' directory
